1ccfa9c87254ec3566a8211a43622f3ded91fc40
panda1 <blueblue0011@gmail.com>
2022-10-11 15:59:03 -0700

    done, added full back up and flask works

---
+++ app.py
@@ -0,0 +1,81 @@
+from re import A
+from flask import Flask, make_response, request
+import json
+import apihelper as a
+import dbhelpers as dh
+
+app = Flask(__name__)
+
+@app.post('/api/hero')
+
+# adding a hero by passing 3 argument(name, description, img_url)
+def add_new_hero():
+    name = request.json.get('name')
+    description = request.json.get('description')
+    img_url = request.json.get('img_url')
+
+    # checking to see if each value that is entered by client is a str, if not it stops there
+    valid_check = a.check_endpoint_info(request.json, ['name', 'description', 'img_url'])
+    if(type(valid_check) == str):
+        return valid_check
+
+    result = dh.run_statement('CALL add_new_hero(?,?,?)', [name, description, img_url])
+    if(type(result) == list):
+        # using make response to make it easier for me to debug
+        # http response 200 = success, 400 = connection problem
+        return make_response(json.dumps(result, default=str), 200)
+      
+    else:
+        return make_response(json.dumps(result, default=str), 400)
+
+@app.get('/api/hero')
+
+def all_hero():
+    result = dh.run_statement('CALL all_hero()')
+    if(type(result) == list):
+        return make_response(json.dumps(result, default=str), 200)
+    else:
+        return make_response(json.dumps(result, default=str), 400)
+
+@app.post('/api/villain')
+
+# adding a villain by passing 3 argument(name, description, img_url)
+def add_new_villain():
+    name = request.json.get('name')
+    description = request.json.get('description')
+    img_url = request.json.get('img_url')
+    hero_id = request.json.get('hero_id')
+
+    # checking to see if each value that is entered by client is a str, if not it stops there
+    valid_check = a.check_endpoint_info(request.json, ['name', 'description', 'img_url', 'hero_id'])
+    if(type(valid_check) == str):
+        return valid_check
+
+    result = dh.run_statement('CALL add_new_villain(?,?,?)', [name, description, img_url, hero_id])
+    if(type(result) == list):
+        # using make response to make it easier for me to debug
+        # http response 200 = success, 400 = connection problem
+        return make_response(json.dumps(result, default=str), 200)
+      
+    else:
+        return make_response(json.dumps(result, default=str), 400)
+
+@app.get('/api/villain')
+def show_all_villain_related_to_hero():
+    hero_id = request.args.get('hero_id')
+
+    valid_check = a.check_endpoint_info(request.args, ['hero_id'])
+    if(type(valid_check) == str):
+        return valid_check
+
+    result = dh.run_statement('CALL show_all_villain_related_hero(?)', [hero_id])
+    if(type(result) == list):
+      
+        # http response 200 = success, 400 = connection problem
+        return make_response(json.dumps(result, default=str), 200)
+      
+    else:
+        return make_response(json.dumps(result, default=str), 400)
+
+
+app.run(debug=True)
---
+++ flask_response_warmup.sql
@@ -0,0 +1,173 @@
+-- MySQL dump 10.13  Distrib 5.5.62, for Win64 (AMD64)
+--
+-- Host: localhost    Database: flask_response_warmup_w22
+-- ------------------------------------------------------
+-- Server version	5.5.5-10.6.9-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `hero`
+--
+
+DROP TABLE IF EXISTS `hero`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `hero` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `description` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `image_url` varchar(200) COLLATE utf8mb4_bin DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `hero`
+--
+
+LOCK TABLES `hero` WRITE;
+/*!40000 ALTER TABLE `hero` DISABLE KEYS */;
+INSERT INTO `hero` VALUES (1,'Saitama','a gag super hero character, that takes one punch to defeat opponent','https://i.imgur.com/jMjo8YV.png');
+/*!40000 ALTER TABLE `hero` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `villain`
+--
+
+DROP TABLE IF EXISTS `villain`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `villain` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(100) COLLATE utf8mb4_bin DEFAULT NULL,
+  `description` varchar(255) COLLATE utf8mb4_bin DEFAULT NULL,
+  `image_url` varchar(200) COLLATE utf8mb4_bin DEFAULT NULL,
+  `hero_id` int(10) unsigned DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `villain_FK` (`hero_id`),
+  CONSTRAINT `villain_FK` FOREIGN KEY (`hero_id`) REFERENCES `hero` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_bin;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `villain`
+--
+
+LOCK TABLES `villain` WRITE;
+/*!40000 ALTER TABLE `villain` DISABLE KEYS */;
+INSERT INTO `villain` VALUES (1,'Boros','Boros may be the first cyberpunk cyclops in anime and along with the striking looks of this villain, Boros boasts quite a powerful skillset. Along with the durability, speed','https://i.imgur.com/Mfq0seu.png',1);
+/*!40000 ALTER TABLE `villain` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping routines for database 'flask_response_warmup_w22'
+--
+/*!50003 DROP PROCEDURE IF EXISTS `add_new_hero` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `add_new_hero`(name_input varchar(100), description_input varchar(255), image_url_input varchar(200))
+    MODIFIES SQL DATA
+begin
+	insert into hero(name, description, image_url)
+	values
+	(name_input, description_input, image_url_input);
+	commit;
+	select last_insert_id(); 
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `add_new_villain` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `add_new_villain`(name_input varchar(100), description_input varchar(255), image_url_input varchar(200), hero_id_input int unsigned)
+    MODIFIES SQL DATA
+begin
+	insert into villain(name, description, image_url)
+	values
+	(name_input, description_input, image_url_input, hero_id_input);
+	commit;
+	select last_insert_id(); 
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `all_hero` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `all_hero`()
+begin
+	select id, name, description, image_url from hero;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!50003 DROP PROCEDURE IF EXISTS `show_all_villain_related_hero` */;
+/*!50003 SET @saved_cs_client      = @@character_set_client */ ;
+/*!50003 SET @saved_cs_results     = @@character_set_results */ ;
+/*!50003 SET @saved_col_connection = @@collation_connection */ ;
+/*!50003 SET character_set_client  = utf8mb4 */ ;
+/*!50003 SET character_set_results = utf8mb4 */ ;
+/*!50003 SET collation_connection  = utf8mb4_unicode_ci */ ;
+/*!50003 SET @saved_sql_mode       = @@sql_mode */ ;
+/*!50003 SET sql_mode              = 'IGNORE_SPACE,STRICT_TRANS_TABLES,ERROR_FOR_DIVISION_BY_ZERO,NO_AUTO_CREATE_USER,NO_ENGINE_SUBSTITUTION' */ ;
+DELIMITER ;;
+CREATE DEFINER=`root`@`localhost` PROCEDURE `show_all_villain_related_hero`(hero_id_input int unsigned)
+begin
+	select id, name, description, image_url from villain
+	where hero_id = hero_id_input;
+END ;;
+DELIMITER ;
+/*!50003 SET sql_mode              = @saved_sql_mode */ ;
+/*!50003 SET character_set_client  = @saved_cs_client */ ;
+/*!50003 SET character_set_results = @saved_cs_results */ ;
+/*!50003 SET collation_connection  = @saved_col_connection */ ;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-10-11 15:58:38
